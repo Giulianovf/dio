138147333bd77b05d3fd77ba16300b05a5b3b32d
Giulianovf <giulianovf@hotmail.com>
2023-06-30 12:14:42 -0300

    add projeto inicial

---
+++ dio/src/App.js
@@ -0,0 +1,17 @@
+import Button from './components/Button'
+
+function App() {
+  return (
+    
+     <div className="App">
+      <h1> olá </h1>
+      <Button title= "Entrar" />
+      <Button title= "Fechar" />
+      <Button title= "Abrir" />
+      <Button title= "Salvar" />
+    </div>
+    
+  );
+}
+
+export default App;
